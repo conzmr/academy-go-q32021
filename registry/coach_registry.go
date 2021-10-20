0299ba41b18c1276f8ae1b3d31e30dbb5fd3f37d
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-10-19 19:55:25 -0500

    Missing comments included in coach registry

--- registry/coach_registry.go
+++ registry/coach_registry.go
@@ -8,18 +8,22 @@ import (
 	"github.com/conzmr/academy-go-q32021/usecase/interactor"
 )
 
+// Returns a NewCoachController from interface
 func (r *registry) NewCoachController() controller.CoachController {
 	return controller.NewCoachController(r.NewCoachInteractor())
 }
 
+// Returns a NewCoachInteractor with a repository and presenter
 func (r *registry) NewCoachInteractor() interactor.CoachInteractor {
 	return interactor.NewCoachInteractor(r.NewCoachRepository(), r.NewCoachPresenter())
 }
 
+// Returns a NewCoachRepository from interface
 func (r *registry) NewCoachRepository() cr.CoachRepository {
 	return ir.NewCoachRepository()
 }
 
+// Returns a NewCoachPresenter from interface
 func (r *registry) NewCoachPresenter() cp.CoachPresenter {
 	return cp.NewCoachPresenter()
 }
