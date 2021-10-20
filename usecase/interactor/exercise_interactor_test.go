3548f55e6b4815891e1fae4d01f6cfff2d149e7a
Constanza Madrigal Reyes <madrigalreyes.c@gmail.com>
2021-10-19 19:55:41 -0500

    Syn test added for exercise interactor

--- usecase/interactor/exercise_interactor_test.go
+++ usecase/interactor/exercise_interactor_test.go
@@ -23,30 +23,70 @@ var exercises = []*model.Exercise{
 	},
 }
 
-type MockRepo struct {
+type ExerciseMockRepo struct {
 	mock.Mock
 }
 
-type MockPresenter struct {
+type ExerciseMockPresenter struct {
 	mock.Mock
 }
 
-func (mr MockRepo) FindAll(exs []*model.Exercise) ([]*model.Exercise, error) {
+func (mr ExerciseMockRepo) FindAll(exs []*model.Exercise, typeId string, items int, itemsPerWorkers int) ([]*model.Exercise, error) {
 	args := mr.Called()
 	return args.Get(0).([]*model.Exercise), args.Error(1)
 }
 
-func (mr MockRepo) Sync(exs []*model.Exercise) ([]*model.Exercise, error) {
+func (mr ExerciseMockRepo) Sync(exs []*model.Exercise) ([]*model.Exercise, error) {
 	args := mr.Called()
 	return args.Get(0).([]*model.Exercise), args.Error(1)
 }
 
-func (mp MockPresenter) ResponseExercises(c []*model.Exercise) []*model.Exercise {
+func (mp ExerciseMockPresenter) ResponseExercises(c []*model.Exercise) []*model.Exercise {
 	args := mp.Called()
 	return args.Get(0).([]*model.Exercise)
 }
 
 func TestFindExercises(t *testing.T) {
+	testCases := []struct {
+		name            string
+		ex              []*model.Exercise
+		hasError        bool
+		error           error
+		id              string
+		typeId          string
+		items           int
+		itemsPerWorkers int
+	}{
+		{
+			name:            "Return exercises list",
+			ex:              exercises,
+			hasError:        false,
+			error:           nil,
+			typeId:          "Odd",
+			items:           3,
+			itemsPerWorkers: 1,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			exerciseMockRepo := ExerciseMockRepo{}
+			exerciseMockPresenter := ExerciseMockPresenter{}
+			exerciseMockRepo.On("FindAll").Return(tc.ex, tc.error)
+			exerciseMockPresenter.On("ResponseExercises").Return(tc.ex)
+			uei := NewExerciseInteractor(exerciseMockRepo, exerciseMockPresenter)
+			exs := []*model.Exercise{}
+			exercises, err := uei.Get(exs, tc.typeId, tc.items, tc.itemsPerWorkers)
+			if tc.hasError {
+				assert.Error(t, err)
+			} else {
+				assert.EqualValues(t, tc.ex, exercises)
+			}
+		})
+	}
+}
+
+func TestSyncExercises(t *testing.T) {
 	testCases := []struct {
 		name     string
 		ex       []*model.Exercise
@@ -55,7 +95,7 @@ func TestFindExercises(t *testing.T) {
 		id       string
 	}{
 		{
-			name:     "Return exercises list",
+			name:     "Return syn exercises",
 			ex:       exercises,
 			hasError: false,
 			error:    nil,
@@ -64,13 +104,13 @@ func TestFindExercises(t *testing.T) {
 
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
-			mockRepo := MockRepo{}
-			mockPresenter := MockPresenter{}
-			mockRepo.On("FindAll").Return(tc.ex, tc.error)
-			mockPresenter.On("ResponseExercises").Return(tc.ex)
-			uei := NewExerciseInteractor(mockRepo, mockPresenter)
+			mockExerciseRepo := ExerciseMockRepo{}
+			mockExercisePresenter := ExerciseMockPresenter{}
+			mockExerciseRepo.On("Sync").Return(tc.ex, tc.error)
+			mockExercisePresenter.On("ResponseExercises").Return(tc.ex)
+			uei := NewExerciseInteractor(mockExerciseRepo, mockExercisePresenter)
 			exs := []*model.Exercise{}
-			exercises, err := uei.Get(exs)
+			exercises, err := uei.Sync(exs)
 			if tc.hasError {
 				assert.Error(t, err)
 			} else {
